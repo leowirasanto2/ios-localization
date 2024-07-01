0d1d27df24c30c6ca6aba53f42f382ba34fac40d
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-07-01 15:53:52 +0800

    setup xcode project

---
+++ IOS Localization/IOS Localization/IOS_LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_LocalizationApp.swift
+//  IOS Localization
+//
+//  Created by Leo Wirasanto Laia on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
